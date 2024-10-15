9adfe6cba4f07c16c91ad762e3adf68cc99f1c58
“kyungsu” <kyungsu77777@gmail.com>
2024-10-15 16:15:33 +0900

    Footer 추가

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="footer">
+      <div>
+        © 2024, Kyungsu Park, All Rights Reserved.
+      </div>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'FooterVue',
+  };
+  </script>
+  
+  <style scoped>
+  .footer {
+    background-color: var(--lightgray-background-color);
+    padding: 30px 0;
+    text-align: center;
+    color: var(--gray-font-color);
+    border-top: 1px solid var(--lightgray-color);
+  }
+  </style>
